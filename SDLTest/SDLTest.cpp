6e051564285f685fb2d43d23f04fc689f96c71ba
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2015-11-04 20:15:21 +0000

    SDLTest now shows Hello World
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@16 e58ad564-698a-4ad4-9377-7572d94e1253

--- SDLTest/SDLTest.cpp
+++ SDLTest/SDLTest.cpp
@@ -1,10 +1,56 @@
+#include <iostream>
+#include <string>
 #include "SDL.h"	//	Include SDL2
+#include "SDL_ttf.h"
+
+SDL_Texture* renderText(const std::string &message, const std::string &fontFile, SDL_Color color, int fontSize, SDL_Renderer *renderer)
+{
+	//	Load TTF font
+	TTF_Font *font = TTF_OpenFont(fontFile.c_str(), fontSize);
+	if (font == nullptr)
+	{
+		SDL_Log(TTF_GetError());
+		return nullptr;
+	}
+
+	SDL_Surface *surface = TTF_RenderText_Blended(font, message.c_str(), color);
+
+	if (surface == nullptr)
+	{
+		SDL_Log("TTF_RenderText_Blended");
+		return nullptr;
+	}
+
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+	if (texture == nullptr)
+	{
+		SDL_Log("SDL_CreateTextureFromSurface");
+		return nullptr;
+	}
+
+	SDL_FreeSurface(surface);
+	TTF_CloseFont(font);
+
+	return texture;
+
+}
 
 int main(int, char** argv)
 {
 	//	Define size of window
 	const int WINDOW_WIDTH = 640;
 	const int WINDOW_HEIGHT = 480;
+
+	//	Define the color white
+	const SDL_Color	WHITE = { 240, 240, 240 };
+
+	//	Define a rectangle positioned in the middle of the screen
+	const SDL_Rect MIDPOINTRECT = { 0, WINDOW_HEIGHT / 2 - 32, 640, 64 };
+
+
+
+
 
 	//	Initialize SDL
 	SDL_Init(SDL_INIT_EVERYTHING);
@@ -22,6 +68,16 @@ int main(int, char** argv)
 	//	If renderer has not been created, exit application
 	if (renderer == NULL)
 		return 0;
+
+	//	Initialize TTF
+	TTF_Init();
+
+	//	Create a texture from a string using provided font
+	SDL_Texture *texture = renderText("Hello World", "C:\\Windows\\Fonts\\tahoma.ttf", WHITE, 24, renderer);
+
+	//	If texture is nullptr it failed to render text
+	if (texture == nullptr)
+		SDL_Log("Texture is null");
 
 	//	Loop forever
 	while (1)
@@ -41,9 +97,13 @@ int main(int, char** argv)
 		SDL_RenderClear(renderer);
 
 		//	Update renderer
+		SDL_RenderCopy(renderer, texture, NULL, &MIDPOINTRECT);
 		SDL_RenderPresent(renderer);
 
 	}
+
+	//	Quit TTF
+	TTF_Quit();
 
 	//	Destroy renderer
 	SDL_DestroyRenderer(renderer);
