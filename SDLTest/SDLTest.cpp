a4ed5b8f82d7b0e6cab4681167f59cfce40c54d1
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2015-11-04 17:22:26 +0000

    Added comments to SDLTest
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@11 e58ad564-698a-4ad4-9377-7572d94e1253

--- SDLTest/SDLTest.cpp
+++ SDLTest/SDLTest.cpp
@@ -1,8 +1,8 @@
-#include "SDL.h"
+#include "SDL.h"	//	Include SDL2
 
 int main(int, char** argv)
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Quit();
-	return 0;
+	SDL_Init(SDL_INIT_EVERYTHING);	//	Try to initialize SDL
+	SDL_Quit();						//	Quit SDL
+	return 0;						//	Return 0 to report success
 }
