96edf21d94383a87fc309a6b73cd2b8199f65047
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2015-12-04 15:47:21 +0000

    Created outline for GenericObject, and example GenericObject.cfg file
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@59 e58ad564-698a-4ad4-9377-7572d94e1253

--- MurrayEngine/GenericObject.cpp
+++ MurrayEngine/GenericObject.cpp
@@ -4,3 +4,74 @@ GenericObject::GenericObject()
 {
 
 }
+
+GenericObject::GenericObject(std::string id, Position currentPosition, TextureAsset* texture, SDL_Rect* rect, double maxSpeed, double acceleration, Orientation orientation)
+{
+	this->id = id;
+	this->currentPosition = currentPosition;
+	this->textureAsset = texture;
+	this->textureRect = rect;
+	this->maxSpeed = maxSpeed;
+	//this->acceleration = acceleration;
+	this->orientation = orientation;
+}
+
+GenericObject::~GenericObject()
+{
+
+}
+
+void GenericObject::setCurrentPosition(int x, int y)
+{
+	this->currentPosition = { x, y };
+}
+
+void GenericObject::setTargetPosition(int x, int y)
+{
+	this->targetPosition = { x, y };
+}
+
+void GenericObject::setOrientation(Orientation orientation)
+{
+	this->orientation = orientation;
+}
+
+void GenericObject::move()
+{
+	//	Move object distance equal to current speed closer to target
+
+	//	Check for collision
+
+	//	If collided, set target position to current position
+
+	//	Perform collision action
+
+	//	Update lastMoved
+}
+
+bool GenericObject::collideBox(GenericObject objectB)
+{
+	//	compare position of object a rect and object b rect
+
+	//	If object a rect placed on object a position overlaps object b rect placed on object b position
+
+		//	return true since objects has collided
+
+	//	Else return false since objects has not collided
+	return false;
+}
+
+bool GenericObject::collidePixel(GenericObject objectB)
+{
+	//	should only be run when collideBox is true
+
+
+
+
+	return false;
+}
+
+void GenericObject::render(SDL_Renderer* renderer)
+{
+
+}
--- MurrayEngine/GenericObject.h
+++ MurrayEngine/GenericObject.h
@@ -2,16 +2,94 @@
 #define _GENERIC_OBJECT_H
 
 #include	<string>
+#include	"Position.h"
+#include	"TextureAsset.h"
+#include	"Timer.h"
 
+enum Orientation
+{
+	NORTH = 0,
+	EAST = 1,
+	SOUTH = 2,
+	WEST = 3
+};
 
 class GenericObject
 {
 private:
 
+	///	Object identifier
+	std::string id;
+
+	///	Current position of object
+	Position	currentPosition;
+
+	///	Target position of object
+	Position	targetPosition;
+
+	///	Texture asset used by object
+	TextureAsset*	textureAsset;
+
+	///	SDL_Rect of texture asset
+	SDL_Rect*		textureRect;
+
+	///	The maximum speed the GenericObject can travel, or 0 for static
+	double			maxSpeed;
+
+	///	The current speed an object is travelling
+	///	double			currentSpeed;
+
+	///	How fast an object reaches its maxSpeed
+	///	double			acceleration;
+
+	///	Current orientation of object
+	Orientation		orientation;
+
+	Timer			timer;
+
+	Uint32			lastRender;
+	Uint32			lastMove;
+
+
 public:
 	GenericObject();
-//	GenericObject(std::string id);
+	GenericObject(std::string id, Position currentPosition, TextureAsset* texture, SDL_Rect* rect, double maxSpeed, double acceleration, Orientation orientation);
 	~GenericObject();
+
+	///	<summary>
+	///	Move (teleport) object to the specified absolute position
+	///	</summary>
+	void	setCurrentPosition(int x, int y);
+
+	///	<summary>
+	///	Move object to the specified relative position
+	///	</summary>
+	void	setTargetPosition(int x, int y);
+
+	///	<summary>
+	///	Sets the orientation of the object
+	///	</summary>
+	void	setOrientation(Orientation orientation);
+
+	///	<summary>
+	///	Move object towards target position
+	///	</summary>
+	void	move();
+
+	///	<summary>
+	///	Detect per-pixel collision between two objects
+	///	</summary>
+	bool	collidePixel(GenericObject objectB);
+
+	///	<summary>
+	///	Detect box collision between two objects
+	///	</summary>
+	bool	collideBox(GenericObject objectB);
+
+	///	<summary>
+	///	Render object in the specified renderer
+	///	</summary>
+	void	render(SDL_Renderer* renderer);
 
 //	std::string getId();
 };
