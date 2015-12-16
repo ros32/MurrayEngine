e39939cb09ce45b89ab21f3fe366ce42a6842845
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2015-12-16 23:24:08 +0000

    Updated documentation for FrameLimiter
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@115 e58ad564-698a-4ad4-9377-7572d94e1253

--- MurrayEngine/FrameLimiter.h
+++ MurrayEngine/FrameLimiter.h
@@ -1,33 +1,64 @@
 #ifndef _FRAME_LIMITER_H
 #define	_FRAME_LIMITER_H
 
-//			Max history value for averaging frame rate
+//			Define the frame rate history sample size.
+//				By limiting frame rate based on average frame rate,
+//				we get a smoother transition between normal operation (with added delay)
+//				and process intensive tasks (where the main loop takes longer than the limit
+//				to execute.
 #define		FRAME_AVG_HISTORY_SAMPLE			2
 
 #include	<SDL.h>
 #include	<vector>
 #include	"Timer.h"
 
+///	<summary>
+///	The Frame Limiter class controls the processing speed of a main loop, 
+///	and ensures that each pass of the loop runs for at least n/1000 ms
+///	(n = frames per second).
+///	</summary>
 class FrameLimiter
 {
 public:
+
+	///	<summary>
+	///	Create a new FrameLimiter with default values
+	///	</summary>
 	FrameLimiter();
+
+	///	<summary>
+	///	Create a new FrameLimiter with a copy of the provided Timer
+	///	and the specified frame limit
+	///	</summary>
 	FrameLimiter(Timer timer, int limit);
+
+
 	~FrameLimiter();
 
-	//	Return average frame rate
+	///	<summary>
+	///	Return the average frame rate. Size of sample is determined by
+	///	the macro FRAME_AVG_HISTORY_SAMPLE
+	///	</summary>
 	Uint32			getAvgFrames();
 
-	//	Return frame limit
+	///	<summary>
+	///	Return the frame limit
+	///	</summary>
 	int				getLimit();
 
-	//	Set frame limit
+	///	<summary>
+	///	Set the frame limit to the specified value
+	///	</summary>
 	void			setLimit(int limit);
 
-	//	Start frame limiter
+	///	<summary>
+	///	Start the frame limiter and begin to measure time
+	///	</summary>
 	void			start();
 
-	//	Calculate delay
+	///	<summary>
+	///	Calculate average frame time and execute delay
+	///	</summary>
 	void			limit();
 
 private:
