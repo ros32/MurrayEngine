bdd7fb8af5fc98710ad9464b6e94c2de9b834bd4
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2015-12-16 22:15:39 +0000

    Updated documentation for Configuration
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@114 e58ad564-698a-4ad4-9377-7572d94e1253

--- MurrayEngine/Configuration.h
+++ MurrayEngine/Configuration.h
@@ -10,26 +10,43 @@
 
 
 ///	<summary>
-///	The Configuration class contain mapped data read from a configuration file
-//	</summary>
+///	The Configuration stores key and value pairs that can be read from and written to file.
+///	Configuration (.cfg) files uses a similiar syntax to INI-files.
+///	</summary>
 class Configuration
 {
 public:
+
+	///	<summary>
+	///	Create an empty Configuration object. 
+	///	</summary>
 	Configuration();
+
+	///	<summary>
+	///	Create a Configuration from the specified file.
+	///	</summary>
 	Configuration(std::string fileName);
+
 	~Configuration();
 
+	//	Typedef iterators for simplification
 	typedef std::map<std::string, std::string>::iterator iterator;
 	typedef std::map<std::string, std::string>::const_iterator const_iterator;
 
-	std::string		fileName;
-
 	///	<summary>
-	///	Get value of a Configuration key
+	///	Get value of a Configuration key. Returns an empty string if value is not found.
 	///	</summary>
 	std::string		getProperty(std::string key);
 
+	///	<summary>
+	///	Get value of a Configuration key, or return defaultValue if key is not found.
+	///	</summary>
 	std::string		getProperty(std::string key, std::string defaultValue);
+
+	///	<summary>
+	///	Get value of a Configuration key and try to parse as int. Return defaultValue
+	///	either if key is not found or parsing failed
+	///	</summary>
 	int				getProperty(std::string key, int defaultValue);
 
 	///	<summary>
@@ -47,6 +64,9 @@ public:
 	///	</summary>
 	void			exportConfig(std::string fileName);
 
+	//	Iterators
+
+	//	We need the iterators begin() and end() to be able to use for(auto key : configuration) on the Configuration object.
 	iterator		begin();
 	const_iterator	begin() const;
 
@@ -54,7 +74,13 @@ public:
 	const_iterator	end() const;
 
 private:
+
+	//	map<string, string> where data is stored
 	std::map<std::string, std::string>		configurationData;
+
+	//	Name of imported file
+	std::string		fileName;
+
 };
 
 #endif // !_CONFIGURATION_H
