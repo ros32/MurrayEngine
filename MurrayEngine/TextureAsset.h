fec403c18adfab05200acd6465710d22381c3967
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2016-01-05 17:10:53 +0000

    Added SDL_Image to platform specific code
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@134 e58ad564-698a-4ad4-9377-7572d94e1253

--- MurrayEngine/TextureAsset.h
+++ MurrayEngine/TextureAsset.h
@@ -3,10 +3,11 @@
 
 #ifdef _WIN32
 #include <SDL.h>
+#include <SDL_image.h>
 #elif __linux__
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #endif
-#include <SDL_image.h>
 #include <vector>
 #include <map>
 #include "Asset.h"
