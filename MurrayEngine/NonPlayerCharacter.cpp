9230255077b12ddb1b3570ff2cd7b88940751e4e
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2016-01-15 10:00:39 +0000

    Added NonPlayerCharacter as a wrapper around Object
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@166 e58ad564-698a-4ad4-9377-7572d94e1253

--- MurrayEngine/NonPlayerCharacter.cpp
+++ MurrayEngine/NonPlayerCharacter.cpp
@@ -7,7 +7,150 @@ NonPlayerCharacter::NonPlayerCharacter()
 
 NonPlayerCharacter::~NonPlayerCharacter()
 {
+	delete this->baseObject;
+}
 
+std::string NonPlayerCharacter::getId()
+{
+	return this->baseObject->getId();
+}
+
+void	NonPlayerCharacter::setId(std::string id)
+{
+	this->baseObject->setId(id);
+}
+
+Position NonPlayerCharacter::getCurrentPosition()
+{
+	return this->baseObject->getCurrentPosition();
+}
+
+void NonPlayerCharacter::setCurrentPosition(int x, int y)
+{
+	this->baseObject->setCurrentPosition(x, y);
+}
+
+void	NonPlayerCharacter::setCurrentPosition(Position pos)
+{
+	this->baseObject->setCurrentPosition(pos);
+}
+
+Position NonPlayerCharacter::getTargetPosition(){
+
+	return this->baseObject->getTargetPosition();
+}
+
+void NonPlayerCharacter::setTargetPosition(int x, int y)
+{
+	this->baseObject->setTargetPosition(x, y);
+}
+
+void	NonPlayerCharacter::setTargetPosition(Position pos)
+{
+	this->baseObject->setTargetPosition(pos);
+}
+
+void NonPlayerCharacter::setOrientation(Orientation orientation)
+{
+	this->baseObject->setOrientation(orientation);
+}
+
+Orientation NonPlayerCharacter::getOrientation()
+{
+	return this->baseObject->getOrientation();
+}
+
+double NonPlayerCharacter::getMaxSpeed()
+{
+	return this->baseObject->getMaxSpeed();
+}
+
+void	NonPlayerCharacter::setMaxSpeed(double speed)
+{
+	this->baseObject->setMaxSpeed(speed);
+}
+
+bool NonPlayerCharacter::getIsCollidable()
+{
+	return this->baseObject->getIsCollidable();
+}
+
+void NonPlayerCharacter::reverseMove()
+{
+	this->baseObject->reverseMove();
+}
+
+void NonPlayerCharacter::move()
+{
+	this->baseObject->move();
+}
+
+bool NonPlayerCharacter::collideBox(Object* objectB)
+{
+	return this->baseObject->collideBox(objectB);
+}
+
+bool NonPlayerCharacter::readAlpha(SDL_Surface* surface, int x, int y)
+{
+	return this->baseObject->readAlpha(surface, x, y);
+}
+
+bool NonPlayerCharacter::collidePixel(Object* objectB)
+{
+	return this->baseObject->collidePixel(objectB);
+}
+
+Texture			NonPlayerCharacter::getTexture()
+{
+	return this->baseObject->getTexture();
+}
+
+void NonPlayerCharacter::render(int x, int y)
+{
+	this->baseObject->render(x, y);
+}
+
+int		NonPlayerCharacter::getCurrentSpeed()
+{
+	return this->baseObject->getCurrentSpeed();
+}
+
+void   NonPlayerCharacter::jump()
+{
+	this->baseObject->jump();
+}
+void   NonPlayerCharacter::doDamage()
+{
+	this->baseObject->doDamage();
+}
+void  NonPlayerCharacter::takeDamage()
+{
+	this->baseObject->takeDamage();
+}
+
+void	NonPlayerCharacter::setTexture(Texture texture)
+{
+	this->baseObject->setTexture(texture);
+}
+
+Uint32	NonPlayerCharacter::getLastRender()
+{
+	return this->baseObject->getLastRender();
+}
+
+Uint32	NonPlayerCharacter::getLastMove()
+{
+	return this->baseObject->getLastMove();
+}
+
+void	NonPlayerCharacter::setLastMove(Uint32 lastMove)
+{
+	this->baseObject->setLastMove(lastMove);
+}
+
+void	NonPlayerCharacter::setLastRender(Uint32 lastRender)
+{
+	this->baseObject->setLastRender(lastRender);
 }
 
 void		NonPlayerCharacter::addAction(Action* action)
--- MurrayEngine/NonPlayerCharacter.h
+++ MurrayEngine/NonPlayerCharacter.h
@@ -9,16 +9,73 @@ class NonPlayerCharacter : public Object
 {
 public:
 	NonPlayerCharacter();
+	NonPlayerCharacter(Object* base);
 	~NonPlayerCharacter();
 
-	virtual void addAction(Action* action);
+	virtual Object*					getBaseObject();
+	virtual void					setBaseObject(Object* base);
 
-	virtual void doAction();
+	virtual AI*						getAI();
+	virtual void					setAI(AI* ai);
+
+	//	Basic
+	virtual std::string				getId();
+	virtual void					setId(std::string id);
+
+	//	Position
+	virtual Position				getCurrentPosition();
+	virtual Position				getTargetPosition();
+	virtual void					setCurrentPosition(Position pos);
+	virtual void					setCurrentPosition(int x, int y);
+	virtual void					setTargetPosition(Position pos);
+	virtual void					setTargetPosition(int x, int y);
+
+	//	Speed
+	virtual int						getCurrentSpeed();
+	virtual double					getMaxSpeed();
+	virtual void					setMaxSpeed(double maxSpeed);
+
+	//	Direction
+	virtual Orientation				getOrientation();
+	virtual void					setOrientation(Orientation orientation);
+
+	//	Texture & Rendering
+	virtual Texture					getTexture();
+	virtual void					setTexture(Texture texture);
+	virtual void					render(int x, int y);
+
+	//	Collision
+	virtual bool					collideBox(Object* objectB);
+	virtual bool					collidePixel(Object* objectB);
+	virtual bool					getIsCollidable();
+
+	// Timer counters
+	virtual Uint32					getLastMove();
+	virtual Uint32					getLastRender();
+	virtual void					setLastRender(Uint32 lastRender);
+	virtual void					setLastMove(Uint32 lastMove);
+
+	//	Actions
+	virtual void					move();
+	virtual void					reverseMove();
+	virtual void					jump();
+	virtual void					doDamage();
+	virtual void					takeDamage();
+
+	//	Action queue
+	virtual void					addAction(Action* action);
+	virtual void					doAction();
+
+	virtual bool readAlpha(SDL_Surface* surface, int x, int y);
+
+protected:
+
+	
 
 private:
 
-	Object*		wrapperObject;
-	AI*					ai;
+	Object*							baseObject;
+	AI*								ai;
 
 	std::queue<Action*>	actionQueue;
 
