29d212cf3c515d96dabd986b313dcfd9da3761f4
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2016-01-16 09:15:37 +0000

    Added operator < to enable Position to be key in std::map
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@171 e58ad564-698a-4ad4-9377-7572d94e1253

--- MurrayEngine/Position.h
+++ MurrayEngine/Position.h
@@ -5,6 +5,12 @@ struct Position
 {
 	int x;
 	int y;
+
+	//	Needed to be key in map
+	bool operator<(const Position& pos) const
+	{
+		return (this->x < pos.x && this->y < pos.y);
+	}
 };
 
 #endif // !_POSITION_H
