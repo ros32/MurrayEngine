cf112bd9c06303d5eb63c14944c8e83672a94c77
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2016-02-29 20:24:38 +0000

    Added comments to MoveAction, added GUIObservable, GUIObserver, GUIText
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@229 e58ad564-698a-4ad4-9377-7572d94e1253

---
+++ MurrayEngine/GUIText.h
@@ -0,0 +1,14 @@
+#ifndef _GUI_TEXT_H
+#define	_GUI_TEXT_H
+
+class GUIText
+{
+public:
+	GUIText();
+	~GUIText();
+
+private:
+
+};
+
+#endif // !_GUI_TEXT_H
--- MurrayEngine/MoveAction.h
+++ MurrayEngine/MoveAction.h
@@ -16,24 +16,76 @@ Triggers the movements of all movable objects
 #endif
 #include "Camera.h"
 
-
+///-------------------------------------------------------------------------------------------------
+/// <summary>	A move action. </summary>
+///
+/// <remarks>	Rosen, 2016-02-29. </remarks>
+///-------------------------------------------------------------------------------------------------
 class MoveAction : public Action{
 
 public:
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Constructor. </summary>
+	///
+	/// <remarks>	Rosen, 2016-02-29. </remarks>
+	///
+	/// <param name="source">   	[in,out] If non-null, source for the. </param>
+	/// <param name="map">			[in,out] If non-null, the map. </param>
+	/// <param name="direction">	The direction. </param>
+	///-------------------------------------------------------------------------------------------------
 	MoveAction(Object* source, Map* map, Orientation direction);
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Constructor. </summary>
+	///
+	/// <remarks>	Rosen, 2016-02-29. </remarks>
+	///
+	/// <param name="source">   	[in,out] If non-null, source for the. </param>
+	/// <param name="map">			[in,out] If non-null, the map. </param>
+	/// <param name="direction">	The direction. </param>
+	/// <param name="repeat">   	The repeat. </param>
+	///-------------------------------------------------------------------------------------------------
 	MoveAction(Object* source, Map* map, Orientation direction, int repeat);
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Destructor. </summary>
+	///
+	/// <remarks>	Rosen, 2016-02-29. </remarks>
+	///-------------------------------------------------------------------------------------------------
 	~MoveAction();
 
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Executes this object. </summary>
+	///
+	/// <remarks>	Rosen, 2016-02-29. </remarks>
+	///-------------------------------------------------------------------------------------------------
 	void execute();
 
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Copies this object. </summary>
+	///
+	/// <remarks>	Rosen, 2016-02-29. </remarks>
+	///
+	/// <returns>	null if it fails, else an Action*. </returns>
+	///-------------------------------------------------------------------------------------------------
 	Action*	copy();
 
 private:
 
+	/// <summary>	The repeat. </summary>
 	int repeat;
+
+	/// <summary>	The current repeat. </summary>
 	int currentRepeat;
+
+	/// <summary>	Source for the. </summary>
 	Object* source;
+
+	/// <summary>	The map. </summary>
 	Map* map;
+
+	/// <summary>	The direction. </summary>
 	Orientation direction = NONE;
 };
 
