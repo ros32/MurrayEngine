f1db0fc0cf7e1e8a5c687a3dc1b46e1067dad641
Nicklas Rosvall Carlquist <nicklas.rosvall@cqst.se>
2016-03-04 20:26:02 +0000

    Added comments to Factory
    git-svn-id: https://cqst.svn.cloudforge.com/prog3ht2015/PROG3Game/trunk@240 e58ad564-698a-4ad4-9377-7572d94e1253

--- MurrayEngine/Factory.h
+++ MurrayEngine/Factory.h
@@ -1,39 +1,128 @@
 #ifndef _FACTORY_H
 #define	_FACTORY_H
 
-#ifdef _WIN32
-#include <SDL.h>
-#elif __linux__
-#include <SDL2/SDL.h>
-#endif
-
-#include	"Configuration.h"
-#include	"Object.h"
-#include	"Tile.h"
-#include	"Map.h"
-#include	"TextureAsset.h"
 #include	"GameInstance.h"
+#include	"Map.h"
 
 class GameInstance;
 class KeyController;
 
+///-------------------------------------------------------------------------------------------------
+/// <summary>	The Factory class contains all factory methods used to create
+/// 			objects in the engine. The Factory class is meant to be
+/// 			extended by the implementation, but contains default
+/// 			implementations of basic functions for testing purposes.</summary>
+///
+/// <remarks>	Rosen, 2016-03-04. </remarks>
+///-------------------------------------------------------------------------------------------------
 class Factory
 {
 public:
-		Factory();
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Default constructor. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///-------------------------------------------------------------------------------------------------
+	Factory();
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Destructor. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///-------------------------------------------------------------------------------------------------
 	virtual ~Factory();
 
-	void	setWindow(SDL_Window* window);
-	void	setGameInstance(GameInstance* instance);
-
-	GameInstance*	getGameInstance();
-	SDL_Window*			getWindow();
-
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Creates an object and returns a pointer to it </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <param name="config">	The configuration. </param>
+	///
+	/// <returns>	null if it fails, else the new object. </returns>
+	///-------------------------------------------------------------------------------------------------
 	virtual Object*						createObject(Configuration config);
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Creates a map. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <param name="config">	The configuration. </param>
+	///
+	/// <returns>	null if it fails, else the new map. </returns>
+	///-------------------------------------------------------------------------------------------------
 	virtual Map*						createMap(Configuration config);
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Creates a TextureAsset object. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <param name="configuration">	The configuration. </param>
+	///
+	/// <returns>	null if it fails, else the new asset. </returns>
+	///-------------------------------------------------------------------------------------------------
 	virtual TextureAsset*				createAsset(Configuration configuration);
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Creates a key controller. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <param name="configuration">	The configuration. </param>
+	///
+	/// <returns>	null if it fails, else the new key controller. </returns>
+	///-------------------------------------------------------------------------------------------------
 	virtual KeyController*				createKeyController(Configuration configuration);
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Creates an animation. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <param name="configuarion">	The configuarion. </param>
+	///
+	/// <returns>	The new animation. </returns>
+	///-------------------------------------------------------------------------------------------------
 	virtual std::shared_ptr<Animation>	createAnimation(Configuration configuarion);
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Gets game instance. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <returns>	null if it fails, else the game instance. </returns>
+	///-------------------------------------------------------------------------------------------------
+	GameInstance*						getGameInstance();
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Sets game instance. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <param name="instance">	[in,out] If non-null, the instance. </param>
+	///-------------------------------------------------------------------------------------------------
+	void								setGameInstance(GameInstance* instance);
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Gets the window. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <returns>	null if it fails, else the window. </returns>
+	///-------------------------------------------------------------------------------------------------
+	SDL_Window*							getWindow();
+
+	///-------------------------------------------------------------------------------------------------
+	/// <summary>	Sets a window. </summary>
+	///
+	/// <remarks>	Rosen, 2016-03-04. </remarks>
+	///
+	/// <param name="window">	[in,out] If non-null, the window. </param>
+	///-------------------------------------------------------------------------------------------------
+	void								setWindow(SDL_Window* window);
 
 private:
 
